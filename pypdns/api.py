36b92bb514dad5ce9b4b83dbf2a3146f253ef221
Sebdraven <sebdraven@protonmail.com>
2021-07-15 17:13:04 +0200

    first commit

---
+++ pypdns/api.py
@@ -0,0 +1,78 @@
+from typing import Dict
+import requests
+import logging
+
+
+class Api_PDNS:
+    def __init__(
+        self,
+        url: str,
+    ) -> None:
+        self.base_url = url
+        self.request = {
+            "GET": requests.get,
+            "POST": requests.post,
+            "PUT": requests.put,
+            "DELETE": requests.delete,
+        }
+        self.headers = {}
+
+    def connect(self, login: str, password: str) -> None:
+        data = {"identity": login, "password": password}
+        url = "%s/login" % self.base_url
+        response = self.__make_requests("POST", url + "/login", data=data)
+        if response.status_code == 200:
+            res = response.json()
+            self.headers = {"Authorization": "Bearer %s" % res["access_token"]}
+        pass
+
+    def get_records(self, domain: str) -> Dict:
+        url = "%s/dn/%s" % (self.base_url, domain)
+        response = self.__make_requests("GET", url, headers=self.headers)
+        if response.status_code == 200:
+            return response.json()
+
+    def add_domain(self, domain: str) -> bool:
+        url = "%s/dn/%s" % (self.base_url, domain)
+        response = self.__make_requests("POST", url, headers=self.headers)
+        if response.status_code == 201:
+            return True
+        return False
+
+    def get_reverse(self, ip: str) -> Dict:
+        url = "%s/reverse/%s" % (self.base_url, ip)
+        response = self.__make_requests("GET", url, headers=self.headers)
+        if response.status_code == 200:
+            return response.json()
+
+    def get_reverse_history(self, ip: str) -> Dict:
+        url = "%s/reverse/%s/history" % (self.base_url, ip)
+        response = self.__make_requests("GET", url, headers=self.headers)
+        if response.status_code == 200:
+            return response.json()
+
+    def get_resolutions_history(self, domain: str) -> Dict:
+        url = "%s/resolution/%s/history" % (self.base_url, domain)
+        response = self.__make_requests("GET", url, headers=self.headers)
+        if response.status_code == 200:
+            return response.json()
+
+    def get_last_resolution(self, domain: str) -> Dict:
+        url = "%s/resolution/%s" % (self.base_url, domain)
+        response = self.__make_requests("GET", url, headers=self.headers)
+        if response.status_code == 200:
+            return response.json()
+
+    def __make_requests(
+        self, method: str, url: str, headers={}, data={}
+    ) -> requests.Response:
+        try:
+            response = None
+            if method == "POST":
+                response = self.request[method](url, json=data, headers=headers)
+            else:
+                response = self.request[method](url, data=data, headers=headers)
+        except KeyError:
+            logging.error("Unsupported method %s " % method)
+
+        return response
---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup
+from setuptools import find_packages
+
+"""Packaging tool for the Yeti python bindings and CLI utility."""
+
+from setuptools import setup
+from setuptools import find_packages
+
+"""Returns contents of README.md."""
+with open("README.md", "r", encoding="utf-8") as readme_fp:
+    long_description = readme_fp.read()
+
+setup(
+    name="pypdns",
+    version="1.0",
+    description="Binding python for passiveDNS Application",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "'License :: OSI Approved :: BSD License'",
+        "Development Status :: 5 - Production/Stable",
+        "Programming Language :: Python :: 3",
+    ],
+    keywords="passiveDNS threat intel api",
+    url="https://github.com/PassivePDNS/pypdns",
+    author="Sebastien Larinier @sebdraven",
+    packages=find_packages(),
+    install_requires=[
+        "requests",
+    ],
+    include_package_data=True,
+    python_requires=">=3.8",
+    zip_safe=False,
+)
